fixed backprop

weenygrad.py
@@ -1,24 +1,37 @@
 import numpy as np
 
 class ADVect():
-    def __init__(self, data, children = []):
-        data = data if isinstance(data, np.ndarray) else np.array(data)
-        self.data =data 
-        self.grad = np.zeros_like(self.data)
-        self.children = set(children)
-        self._backward = lambda: None 
+    def __init__(self, data, _children = []):
+        data            = data if isinstance(data, np.ndarray) else np.array(data)
+        self.data       = data 
+        self.grad       = np.zeros_like(self.data)
+        self._children  = set(_children)
+        self._backward  = lambda: None 
     
     def __add__(self, other):
-        other = other if isinstance(other, ADVect) else ADVect(other)
-        out = ADVect(self.data+ other.data, [self, other])
-        def backward():
+        other           = other if isinstance(other, ADVect) else ADVect(other)
+        out             = ADVect(self.data+ other.data, [self, other])
+        def _backward():
             if self == other:
-                self.grad = other.grad + out.grad
+                self.grad   = other.grad + out.grad
             else:
-                self.grad = self.grad + out.grad
-                other.grad = other.grad + out.grad
-        out._backward = backward
+                self.grad   = self.grad + out.grad
+                other.grad  = other.grad + out.grad
+        out._backward   = _backward
         return out
+    
+    def __sub__(self, other):
+        other           = other if isinstance(other, ADVect) else ADVect(other)
+        out             = ADVect(self.data+ other.data, [self, other])
+        def _backward():
+            if self == other:
+                self.grad   = other.grad + out.grad
+            else:
+                self.grad   = self.grad + out.grad
+                other.grad  = other.grad - out.grad
+        out._backward   = _backward
+        return out
+
 
     def __matmul__(self, other):
         other = other if isinstance(other, ADVect) else ADVect(other)
@@ -26,45 +39,46 @@ class ADVect():
             out = ADVect(self.data * other.data, [self, other]) 
         else: 
             out = ADVect(self.data @ other.data, [self, other]) 
-        def backward():
+
+        def _backward():
             if np.ndim(self.data) == 1:
-                self.grad   = self.grad +   out.grad * other.data
-                other.grad  = other.grad +  out.grad * self.data
+                self.grad   = self.grad + out.grad * other.data
+                other.grad  = other.grad + out.grad * self.data
             else: # Matrix vector mulitplication is supported, not matrix @ matrix
-                self.grad = self.grad + np.outer(out.grad, other.data) 
-                other.grad = other.grad + np.transpose(self.data) @ out.grad
-        out._backward = backward
+                self.grad   = self.grad + np.outer(out.grad, other.data) 
+                other.grad  = other.grad + np.transpose(self.data) @ out.grad
+
+        out._backward = _backward
         return out
 
     def sum(self):
         out = ADVect(np.sum(self.data), [self])
-        def backward():
+        def _backward():
             self.grad = np.ones_like(self.grad)*out.grad
-        out._backward = backward   
+        out._backward = _backward   
         return out
 
     def relu(self):
         ReLU = np.vectorize(lambda x: max(0, x))
         out = ADVect(ReLU(self.data), [self])
-        def backward():
-            self.grad = self.grad + (out.data >= 0) * out.grad
-        out._backward = backward
+        def _backward():
+            self.grad = self.grad + (out.data > 0) * out.grad
+        out._backward = _backward
         return out
 
     def backward(self):
-        topo, visited = [], []
+        topo, visited = [], set() 
         def build_topo(v):
             if v not in visited:
+                visited.add(v)
+                for child in v._children:
+                    build_topo(child)
                 topo.append(v)
-                visited.append(v)
-                for node in v.children:
-                    build_topo(node)
-                return topo
-            return []
-        self.grad = np.ones_like(self.data)
         build_topo(self)
-        for v in topo:
+        self.grad = np.ones_like(self.data)
+        for v in reversed(topo):
             v._backward()
+
 
     def __radd__(self, other):
         return self + other
@@ -73,7 +87,7 @@ class ADVect():
         return self @ other
     
     def __repr__(self):
-        return f'data:\n{self.data},\n grad: {self.grad}'
+        return f'data:\n{self.data},\n grad: {self.grad}\n'
 
 class Module():
     def params(self):

weenygrad_test.py
@@ -2,35 +2,21 @@ import numpy as np
 from weenygrad import ADVect
 import torch
 
-def test_ops():
-    a = ADVect(np.arange(3))
-    b = ADVect(np.arange(3)+3)
-    c = ADVect(np.arange(3)+6)
-    d = ADVect(np.arange(1)+6)
+def test_addition():
+    a = ADVect(np.arange(10))
+    b = ADVect(np.arange(10)-14)
+    c = a + b + b
+    c.backward()
+    awg, cwg = a, c
 
-    e = c @ b
-    f = e + d
-    g = f.relu()
-    g.grad = 1
+    a = torch.Tensor(np.arange(10))
+    a.requires_grad = True
+    b = torch.Tensor(np.arange(10)-14)
+    c = a + b + b
+    apt, cpt = a, c
+    assert cwg.data == cpt.data.item()
+    assert awg.grad == apt.grad.item()
 
-    g._backward()
-    f._backward()
-    d._backward()
-    e._backward()
-    for v in [g,f,d,e,c,b]:
-        print(v)
-
-def test_backward():
-    a = ADVect(np.arange(3))
-    b = ADVect(np.arange(3)+3)
-    c = ADVect(np.arange(3)+6)
-    d = ADVect(np.arange(1)+6)
-    e = c @ b
-    f = e + d
-    g = f.relu()
-    g.backward()
-    for v in [g,f,d,e,c,b]:
-        print(v)
 
 def test_matrix_vector_multiply():
     A = np.arange(6)
@@ -38,22 +24,25 @@ def test_matrix_vector_multiply():
     b = ADVect(np.arange(2)+6)
     c = A @ b
     c.backward()
-    Awg, cwg = A, c 
+    d = c.sum()
+    Awg, dwg = A, d
 
     A = np.arange(6)
     A = A.reshape(3,2)
     A = torch.Tensor(A)
+    A.requires_grad = True
     b = torch.Tensor(np.arange(2)+6)
     c = A @ b
-    c.backward()
-    Apt, cpt = A, c
+    d = c.sum()
+    d.backward()
+    Apt, dpt = A, d
 
     # forward pass is correct
-    assert cwg.data == cpt.data.item()
+    assert dwg.data == dpt.data.item()
     # backward pass is correct
-    assert Awg.grad == Apt.grad.item()
+    assert np.allclose(Awg.grad, Apt.grad.numpy())
 
-def test_sanity():
+def test_sanity(DEBUG=False):
     x = ADVect([-4.0])
     z = [2.0] @ x + [2.0] + x
     q = z.relu() + z @ x
@@ -72,12 +61,14 @@ def test_sanity():
     y = q @ x
     y.backward()
     xpt, ypt = x, y
+    if DEBUG:
+        print(xwg.grad)
+        print(xpt.grad.item())
 
-    # forward pass is correct
     assert ywg.data == ypt.data.item()
-    # backward pass is correct
     assert xwg.grad == xpt.grad.item()
 
 if __name__ == '__main__':
-    test_sanity()
-    #test_matrix_vector_multiply()
+    test_addition
+    test_matrix_vector_multiply()
+    test_sanity() 